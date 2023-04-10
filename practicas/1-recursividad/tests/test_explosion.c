b51c5eda414c704a3683f98e317a535b93ad3bdc
Mateo Calvo <matucalv@gmail.com>
2023-04-10 10:04:52 -0300

    .

--- practicas/1-recursividad/tests/test_explosion.c
+++ practicas/1-recursividad/tests/test_explosion.c
@@ -10,12 +10,14 @@ void probar(int n, int b, int *esperado){
     int i;
     int size = 0;
     int * res = explosion(n,b,&size);
-    printf("fragmentos(%d,%d): ", n, b);
+    printf("fragmentos(%d,%d):", n, b);
 
     for (i = 0; i < size; i++)
     {
-        printf("%d", res[i]);
+        printf(" %d ", res[i]);
     }
+
+    
     
 
     int_array_eq(res,esperado,sizeof(res),sizeof(esperado));
@@ -33,12 +35,13 @@ int main() {
     int arreglo1[] = {4, 3, 2, 2, 1, 1, 1, 1, 5};
     int arreglo2[] = {3, 2, 1, 1, 3};
 
-
 // tests propuestos
 
     probar(20,5,arreglo1);
     printf("\n");
     probar(10,3,arreglo2);
+    
+
    
 
     
