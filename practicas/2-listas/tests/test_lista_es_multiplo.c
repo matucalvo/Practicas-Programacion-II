60eb435b0772b43ff443077b03e23ba55ffbf728
Yonahuel <yonahuel2006@hotmail.com>
2023-04-21 11:54:27 -0300

    Ejercicio múltiplos

--- practicas/2-listas/tests/test_lista_es_multiplo.c
+++ practicas/2-listas/tests/test_lista_es_multiplo.c
@@ -3,6 +3,25 @@
 #include "tipo_elemento.h"
 #include "listas/listas.h"
 #include "cadenas.h"
+#include "util_listas.h"
+#include "aserciones.h"
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void probarListas(Lista l, int n, Lista listaEsperada){
+    Lista l_multiplos = multiplos(l, n);
+
+    printf("Lista suministrada: ");
+    l_mostrar(l);
+
+    printf("Lista multiplos: ");
+    l_mostrar(l_multiplos);
+
+    assert(l_multiplos, listaEsperada);
+
+    free(l_multiplos);
+}
 
 int main() {
     imprimir_titulo("Tests para determinar si una lista es múltiplo de otra");
